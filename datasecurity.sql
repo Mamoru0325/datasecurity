f29188e048b023fa40e2e7560448734069920c69
MamoruTFN <78528062+MamoruTFN@users.noreply.github.com>
2023-10-05 12:22:09 +0700

    Create datasecurity.sql

---
+++ datasecurity.sql
@@ -0,0 +1,164 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 05, 2023 at 07:07 AM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `datasecurity`
+--
+CREATE DATABASE IF NOT EXISTS `datasecurity` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+USE `datasecurity`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cipher`
+--
+
+CREATE TABLE `cipher` (
+  `cipherId` int(11) NOT NULL,
+  `typeId` int(11) NOT NULL,
+  `plainText` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `cipherKey` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `level` enum('easy','moderate','hard') COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `scoreboard`
+--
+
+CREATE TABLE `scoreboard` (
+  `scoreId` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `level` enum('easy','moderate','hard') COLLATE utf8_unicode_ci NOT NULL,
+  `score` int(11) NOT NULL DEFAULT 0,
+  `time` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `type`
+--
+
+CREATE TABLE `type` (
+  `typeId` int(11) NOT NULL,
+  `typeName` varchar(50) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `type`
+--
+
+INSERT INTO `type` (`typeId`, `typeName`) VALUES
+(1, 'caesar'),
+(2, 'vigenere'),
+(3, 'permutation');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `userId` int(11) NOT NULL,
+  `email` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `username` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `phoneNumber` varchar(11) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cipher`
+--
+ALTER TABLE `cipher`
+  ADD PRIMARY KEY (`cipherId`),
+  ADD KEY `typeId` (`typeId`);
+
+--
+-- Indexes for table `scoreboard`
+--
+ALTER TABLE `scoreboard`
+  ADD PRIMARY KEY (`scoreId`),
+  ADD KEY `userId` (`userId`);
+
+--
+-- Indexes for table `type`
+--
+ALTER TABLE `type`
+  ADD PRIMARY KEY (`typeId`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`userId`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cipher`
+--
+ALTER TABLE `cipher`
+  MODIFY `cipherId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `scoreboard`
+--
+ALTER TABLE `scoreboard`
+  MODIFY `scoreId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `type`
+--
+ALTER TABLE `type`
+  MODIFY `typeId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `cipher`
+--
+ALTER TABLE `cipher`
+  ADD CONSTRAINT `cipher_ibfk_1` FOREIGN KEY (`typeId`) REFERENCES `type` (`typeId`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
+-- Constraints for table `scoreboard`
+--
+ALTER TABLE `scoreboard`
+  ADD CONSTRAINT `scoreboard_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `user` (`userId`) ON DELETE NO ACTION ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
