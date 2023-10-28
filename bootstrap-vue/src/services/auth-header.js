d7d7494c0d915cd27db9a9a8dc83347293491450
klapawee <78515310+b6320502436@users.noreply.github.com>
2023-10-28 21:04:24 +0700

    up auth

--- bootstrap-vue/src/services/auth-header.js
+++ bootstrap-vue/src/services/auth-header.js
@@ -1,7 +1,13 @@
 export default function authHeader() {
   let user = JSON.parse(localStorage.getItem('user'));
-
+  let timer = JSON.parse(localStorage.getItem('timer'));
   if (user && user.token) {
+    return { Authorization: 'Bearer ' + user.token }; // for Spring Boot back-end
+    // return { 'x-access-token': user.token };       // for Node.js Express back-end
+  } else {
+    return {};
+  }
+  if (timer ) {
     return { Authorization: 'Bearer ' + user.token }; // for Spring Boot back-end
     // return { 'x-access-token': user.token };       // for Node.js Express back-end
   } else {
