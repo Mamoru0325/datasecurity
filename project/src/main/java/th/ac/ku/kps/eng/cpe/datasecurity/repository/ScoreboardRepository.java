c4fe85ff02caf4ca5dba4a08740668dd92de475b
MamoruTFN <78528062+MamoruTFN@users.noreply.github.com>
2023-10-28 22:53:36 +0700

    update

--- project/src/main/java/th/ac/ku/kps/eng/cpe/datasecurity/repository/ScoreboardRepository.java
+++ project/src/main/java/th/ac/ku/kps/eng/cpe/datasecurity/repository/ScoreboardRepository.java
@@ -12,9 +12,7 @@ import th.ac.ku.kps.eng.cpe.datasecurity.model.Scoreboard;
 
 @Repository
 public interface ScoreboardRepository extends CrudRepository<Scoreboard, Integer> {
-	
-	@Query("select s.scoreId, u.username, max(s.score) from Scoreboard s inner join s.user u where u in "
-			+ "(select s.user from Scoreboard s where s.level = :level order by s.score desc) and s.level = :level "
-			+ "group by u.userId")
-	public List<Object> findByLevelOrderByscoreDESC(@Param("level")String level, Pageable pageable);
+
+	@Query("select s.scoreId, u.username, max(s.score) from Scoreboard s inner join s.user u where s.level = :level group by u.userId order by max(s.score) desc")
+	public List<Object> findByLevelOrderByscoreDESC(@Param("level") String level, Pageable pageable);
 }
