658016c1c3c6bdd0184b590d99ec1784d1f5c826
Nitiphat2 <78531262+Nitiphat2@users.noreply.github.com>
2023-11-03 12:50:39 +0700

    Update HelloWorld.vue

--- bootstrap-vue/src/components/HelloWorld.vue
+++ bootstrap-vue/src/components/HelloWorld.vue
@@ -3,8 +3,8 @@
     <h1 style="text-align: center;">{{ msg }}</h1>
     <br>
     <div class="row">
-      <div class="card" style="width: 22rem; margin: auto; border: 1px solid #000;">
-        <img src="https://html.com/wp-content/uploads/flamingo.webp" class="card-img-top" alt="...">
+      <div class="card" style="width: 25rem; margin: auto; border: 1px solid #000;">
+        <img src="https://html.com/wp-content/uploads/flamingo.webp" class="card-img-top" alt="..." style="margin-top: 3%">
         <div class="card-body">
           <h5 class="card-title">การถอดรหัสแบบซีซาร์</h5>
           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
@@ -12,8 +12,8 @@
             <RouterLink to="/cizar"><a class="btn btn-primary" style="color:#000000">อ่านเพิ่มเติม</a></RouterLink>
         </div>
       </div>
-      <div class="card" style="width: 22rem; margin: auto; border: 1px solid #000;">
-        <img src="https://html.com/wp-content/uploads/flamingo.webp" class="card-img-top" alt="...">
+      <div class="card" style="width: 25rem; margin: auto; border: 1px solid #000;">
+        <img src="https://html.com/wp-content/uploads/flamingo.webp" class="card-img-top" alt="..." style="margin-top: 3%">
         <div class="card-body">
           <h5 class="card-title">การถอดรหัสแบบ Vigenere</h5>
           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
@@ -21,8 +21,8 @@
             <RouterLink to="/vigenere"><a class="btn btn-primary" style="color:#000000">อ่านเพิ่มเติม</a></RouterLink>
         </div>
       </div>
-      <div class="card" style="width: 22rem; margin: auto; border: 1px solid #000;">
-        <img src="https://html.com/wp-content/uploads/flamingo.webp" class="card-img-top" alt="...">
+      <div class="card" style="width: 25rem; margin: auto; border: 1px solid #000;">
+        <img src="https://html.com/wp-content/uploads/flamingo.webp" class="card-img-top" alt="..." style="margin-top: 3%">
         <div class="card-body">
           <h5 class="card-title">การถอดรหัสแบบ Permutation</h5>
           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
